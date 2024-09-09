dd0c37e96c002c5697b8e8d45ec4445f19a3ecaf
Luis Urzúa <lurzua@softup.cl>
2024-09-09 18:45:18 -0300

    marcadores dinamicos agregados

--- assets/script/DrugStoreService.mjs
+++ assets/script/DrugStoreService.mjs
@@ -1,5 +1,5 @@
 
-const getDrugStore = async() => {
+const getDrugStore = async () => {
     const response = await fetch('https://midas.minsal.cl/farmacia_v2/WS/getLocalesTurnos.php');
     let data = await response.json();
 
@@ -7,6 +7,27 @@ const getDrugStore = async() => {
 
 }
 
+const getDrugstoreNearby = (farmaciasCercanas) => {
+    const farmaciaCercana = farmaciasCercanas[0];
+    let map = L.map('map').setView([farmaciaCercana.local_lat, farmaciaCercana.local_lng], 15);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 45,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    for (let i = 0; i < 5; i++) {
+        const { local_lat, local_lng, local_nombre ,local_direccion,local_telefono} = farmaciasCercanas[i]
+        L.marker([local_lat, local_lng]).addTo(map)
+        .bindPopup(`Farmacia: ${local_nombre}<br> 
+                    Dirección: ${local_direccion}<br>
+                    Teléfono: ${local_telefono}<br>
+        `).openPopup();
+    }
+
+}
+
 export {
-    getDrugStore, 
+    getDrugStore,
+    getDrugstoreNearby
 }
--- assets/script/script.mjs
+++ assets/script/script.mjs
@@ -1,5 +1,5 @@
-import { getDrugStore } from './DrugStoreService.mjs';
-import { ordenLatitud, ordenDesdeSantiago, getClosestPoints } from './Order.mjs'
+import { getDrugStore, getDrugstoreNearby } from './DrugStoreService.mjs';
+import { getClosestPoints } from './Order.mjs'
 
 navigator.geolocation.getCurrentPosition(
     (position) => {
@@ -7,21 +7,7 @@ navigator.geolocation.getCurrentPosition(
             .then(data => {
                 return data.sort((farmacia1, farmacia2) => getClosestPoints(farmacia1, farmacia2, position.coords));
             })
-            .then(farmaciaOrdenada => {
-                const farmaciaCercana = farmaciaOrdenada[0];
-                console.log({farmaciaCercana})
-                let map = L.map('map').setView([farmaciaCercana.local_lat, farmaciaCercana.local_lng], 15);
-
-                L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                    maxZoom: 45,
-                    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-                }).addTo(map);
-
-                let marker = L.marker([farmaciaCercana.local_lat, farmaciaCercana.local_lng]).addTo(map);
-                 marker.bindPopup(`Farmacia: ${farmaciaCercana.local_nombre} <br> Dirección: ${farmaciaCercana.local_direccion}  `).openPopup();
-
-            }
-            )
+            .then(farmaciaOrdenada => getDrugstoreNearby(farmaciaOrdenada))
     },
     () => {
         alert('No pudimos acceder a tu ubicación')
